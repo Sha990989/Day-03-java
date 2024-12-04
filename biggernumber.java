ad5698fe3ed146525a1f26c6adcbc8df4381b375
Sha990989 <anavaneethasankar@gmail.com>
2024-12-04 14:42:31 +0530

    Add files via upload

---
+++ biggernumber.java
@@ -0,0 +1,19 @@
+public class biggernumber{
+    public static void main(String args[])
+    {
+        int a=23,b=45,c=6;
+        if((a>b)&&(a>c))
+        {
+            System.out.println("a is bigger");
+        }
+        else if((b>a)&&(b>c))
+        {
+            System.out.println("b is bigger");
+        }
+        else
+        {
+            System.out.println("c is bigger");
+        }
+    }
+    
+}
