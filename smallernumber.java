89e907776328a15d26f2b58df42eaad4171cafe1
Sha990989 <anavaneethasankar@gmail.com>
2024-12-04 16:21:52 +0530

    Add files via upload

---
+++ smallernumber.java
@@ -0,0 +1,19 @@
+public class smallernumber {
+    public static void main(String args[])
+    {
+        int a=23,b=45,c=6;
+        if((a<b)&&(a<c))
+        {
+            System.out.println("a is smaller");
+        }
+        else if((b<a)&&(b<c))
+        {
+            System.out.println("b is smaller");
+        }
+        else
+        {
+            System.out.println("c is smaller");
+        }
+    }
+    
+}
