2e5927ee130b81bfb60882ddd24e53724c87a486
Sha990989 <anavaneethasankar@gmail.com>
2024-12-04 12:08:30 +0530

    Add files via upload

---
+++ power2.java
@@ -0,0 +1,14 @@
+public class power2 {
+    public static void main(String args[])
+    {
+        int n=44;
+        if((n&(n-1))==0)
+        {
+            System.out.println("yes it's a power");
+        }
+        else{
+            System.out.println("no it is not a power");
+        }
+    }
+    
+}
